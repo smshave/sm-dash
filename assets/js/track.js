09edf42e5ad25d331a8a0e4adb4b8c48a2890209
binc0 <42704408+binc0@users.noreply.github.com>
2019-08-24 21:15:47 +0200

    Update track.js

--- assets/js/track.js
+++ assets/js/track.js
@@ -1,7 +1,7 @@
 
 
 
-$("#trackingNumber").keyup(function(event) {
+$("#trackingNumber").keyup(function (event) {
     if (event.keyCode === ('Enter')) {
   alert("Button code executed.");
     }
@@ -30,45 +30,52 @@ var settings = {
 
 $.ajax(settings).done(function (jcontent){
     
+    	// Time last
+	var d1 = new Date(jcontent.shipments[0].status.timestamp);
+var formattedDate = d1.getDate() + "-" + (d1.getMonth() + 1) + "-" + d1.getFullYear();
+var hours = (d1.getHours() < 10) ? "0" + d1.getHours() : d1.getHours();
+var minutes = (d1.getMinutes() < 10) ? "0" + d1.getMinutes() : d1.getMinutes();
+var formattedTime = hours + ":" + minutes;
+	formattedDate = formattedDate + " " + formattedTime;
     
-    
+    //Last color
     var color1 = "badge badge-pill badge-primary" ;
 var logo = "ni ni-spaceship" ;
-var bagde = "badge badge-pill badge-primary" ;   
+ bagde = "badge badge-pill badge-primary" ;   
 
   var event = jcontent.shipments[0].status.description;
 
 if (event == 'Spedizione in consegna'|| event == 'With delivery courier'){
     var colork = "-info" ;
-    var logo = "ni ni-delivery-fast" ;
-    var color1 = "badge badge-pill badge-info" ;
-    var bagde = "badge badge-pill badge-info" ;
+    logo = "ni ni-delivery-fast" ;
+    color1 = "badge badge-pill badge-info" ;
+    bagde = "badge badge-pill badge-info" ;
     }
 else if (event == 'Spedizione ritirata' || event == 'Shipment picked up'  || event.includes("istruzioni") || event.includes("instruction")) {
-    var logo = "ni ni-box-2";
-    var colork = "-default" ;
-    var color1 = "badge badge-pill badge-default" ;
-    var bagde = "badge badge-pill badge-default" ;
+    logo = "ni ni-box-2";
+    colork = "-default" ;
+    color1 = "badge badge-pill badge-default" ;
+    bagde = "badge badge-pill badge-default" ;
 }
 else if (event.includes("Spedizione Consegnata") || event.includes("Delivered")){
-    var colork = "-success"  ;
-    var color1 = "badge badge-pill badge-success" ;
-    var logo = "ni ni-check-bold" ;
-    var bagde = "badge badge-pill badge-success" ;
+    colork = "-success"  ;
+    color1 = "badge badge-pill badge-success" ;
+    logo = "ni ni-check-bold" ;
+    bagde = "badge badge-pill badge-success" ;
 }
 else if (event.includes("Spedizione arrivata") || event.includes("Arrived at Delivery")){
-    var colork = "-warning"  ;
-    var logo = "ni ni-pin-3" ;
+    colork = "-warning"  ;
+    logo = "ni ni-pin-3" ;
 }
 else if (event == 'Errore' || event == 'Error'){
-    var colork = "-allert"  ;
-    var logo = "ni ni-bell-55" ;
+    colork = "-allert"  ;
+    logo = "ni ni-bell-55" ;
 }
 
-else  var colork =  "-primary" ;
+else  colork =  "-primary" ;
    
    var last = document.getElementById('last');
-last.innerHTML += '<div class="card card-stats"><div class="card-body"><div class="row"><div class="col"><h5 class="card-title text-uppercase text-muted mb-0" id>' + jcontent.shipments[0].status.timestamp + '</h5><span class="h2 text' + colork + ' font-weight-bold mb-0">' + jcontent.shipments[0].status.description + '</span></div><div class="col-auto"><div class="icon icon-shape bg' + colork + ' text-white rounded-circle shadow"><i class="' + logo + '"></i></div></div></div><p class="mt-3 mb-0 text-sm"><span class="text mr-2"> Numero spedizione : </i></span><span class="text-nowrap">' + jcontent.shipments[0].id + '</span></p><p class="mt-3 mb-0 text-sm"> <span class="text-success mr-2"><i class="fas fa-map-marker-alt"></i> Da : </span> <span class="text-nowrap">' + jcontent.shipments[0].origin.address.addressLocality + '</span> </p><p class="mt-3 mb-0 text-sm"> <span class="text-success mr-2"><i class="fas fa-map-marker-alt"></i> A : </span> <span class="text-nowrap">' + jcontent.shipments[0].destination.address.addressLocality + '</span> </p></div></div>';
+last.innerHTML += '<div class="card card-stats"><div class="card-body"><div class="row"><div class="col"><h5 class="card-title text-uppercase text-muted mb-0" id>' + formattedDate + '</h5><span class="h2 text' + colork + ' font-weight-bold mb-0">' + jcontent.shipments[0].status.description + '</span></div><div class="col-auto"><div class="icon icon-shape bg' + colork + ' text-white rounded-circle shadow"><i class="' + logo + '"></i></div></div></div><p class="mt-3 mb-0 text-sm"><span class="text mr-2"> Numero spedizione : </i></span><span class="text-nowrap">' + jcontent.shipments[0].id + '</span></p><p class="mt-3 mb-0 text-sm"> <span class="text-success mr-2"><i class="fas fa-map-marker-alt"></i> Da : </span> <span class="text-nowrap">' + jcontent.shipments[0].origin.address.addressLocality + '</span> </p><p class="mt-3 mb-0 text-sm"> <span class="text-success mr-2"><i class="fas fa-map-marker-alt"></i> A : </span> <span class="text-nowrap">' + jcontent.shipments[0].destination.address.addressLocality + '</span> </p></div></div>';
    
 
 
@@ -76,7 +83,6 @@ var output = document.getElementById('id');
 output.innerHTML = 'Spedizione : ' + jcontent.shipments[0].id;
 
 for (var i = 0; i < jcontent.shipments[0].events.length; i++) {
-    
     
   var color1 = "badge badge-pill badge-primary" ;
 var logo = "ni ni-spaceship" ;
@@ -117,11 +123,18 @@ else if (event == 'Errore' || event == 'Error'){
 
 else  var color =  "timeline-step badge-primary" ;
 
-    
+
+        // Time detail
+d1 = new Date(jcontent.shipments[0].events[i].timestamp);
+formattedDate = d1.getDate() + "-" + (d1.getMonth() + 1) + "-" + d1.getFullYear();
+hours = (d1.getHours() < 10) ? "0" + d1.getHours() : d1.getHours();
+minutes = (d1.getMinutes() < 10) ? "0" + d1.getMinutes() : d1.getMinutes();
+formattedTime = hours + ":" + minutes;
+formattedDate = formattedDate + " " + formattedTime;
     
     
 var output1 = document.getElementById('output');
-output1.innerHTML += '<div class="timeline-block"><span class="' + color + '"><i class="' + logo + '"></i></span><div class="timeline-content"><small class="text-muted font-weight-bold">' + jcontent.shipments[0].events[i].timestamp + '</small><h5 class=" mt-3 mb-0">' + jcontent.shipments[0].events[i].location.address.addressLocality + '</h5><p class=" text-sm mt-1 mb-0">' + jcontent.shipments[0].events[i].description + '</p><div class="mt-3"><span class="' + color1 + '">' + jcontent.shipments[0].id + '</span></div></div></div>';
+output1.innerHTML += '<div class="timeline-block"><span class="' + color + '"><i class="' + logo + '"></i></span><div class="timeline-content"><small class="text-muted font-weight-bold">' + formattedDate + '</small><h5 class=" mt-3 mb-0">' + jcontent.shipments[0].events[i].location.address.addressLocality + '</h5><p class=" text-sm mt-1 mb-0">' + jcontent.shipments[0].events[i].description + '</p><div class="mt-3"><span class="' + color1 + '">' + jcontent.shipments[0].id + '</span></div></div></div>';
 
 
 document.getElementById("trackingNumber").value = jcontent.shipments[0].id;
