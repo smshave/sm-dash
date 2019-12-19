78b354adf904e9e4da255a32a81e7e389b89dc8d
binc0 <42704408+binc0@users.noreply.github.com>
2019-12-19 17:24:22 +0100

    Update track.js

--- assets/js/track.js
+++ assets/js/track.js
@@ -23,7 +23,7 @@ var settings = {
   "url": "https://api-eu.dhl.com/track/shipments" + trackid,
   "method": "GET",
   "headers": {
-    "DHL-API-Key": "${{ secrets.Consumer_Key }}",
+    "DHL-API-Key": "Consumer_Key",
     "Content-Type": "application/json",
   },
   "data": ""
